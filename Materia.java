ba0081db581baa171967960f7152397a5e24fa8c
Kim Viberti <vibertikim@yahoo.it>
2019-03-06 17:24:22 +0100

    Aggiunte a classe Materia.java

--- Materia.java
+++ Materia.java
@@ -1,20 +1,39 @@
-
+//anno\mese\giorno oraInizio:mntInizio-oraFine:mntFine oreEffettiveh minutiEffettivimnt "Materia"\ramo{Persona1, Persona2...}[luogo] 
 public class Materia {
     private int oreTot, oreTeor, orePrat;
-
+    private String nomeMateria; //indeciso se mettere final (pero' in caso in cui uno scrive un nome sbagliato poi bisogna cambiare)
+    private String[][][] calendario;
+    //costruttori----------------------------------------------------------------------------------------------------------------------
     public Materia() {
-
+        setOreTot(0);
+        setOrePrat(0);
+        setOreTeor(0);
+        setNomeMateria("");
     }
 
-    private void setOreTot() {
-
+    public Materia(String name) {
+        setOreTot(0);
+        setOrePrat(0);
+        setOreTeor(0);
+        setNomeMateria(name);
     }
+    //set e get------------------------------------------------------------------------------------------------------------------------
+    private void setOreTot(int n) {oreTot = n; /*this. e' opzionale*/}
 
-    private void setOreTeor() {
+    private void setOreTeor(int n) {oreTeor = n; /*this. e' opzionale*/}
 
-    }
+    private void setOrePrat(int n) {orePrat = n; /*this. e' opzionale*/}
 
-    private void setOrePrat() {
+    private void setNomeMateria(String str) {nomeMateria = str; /*this. e' opzionale*/}
 
-    }
+    public int getOreTot() {return oreTot; /*this. e' opzionale*/}
+
+    public int getOreTeor() {return oreTeor; /*this. e' opzionale*/}
+
+    public int getOrePrat() {return orePrat; /*this. e' opzionale*/}
+
+    public String getNomeMateria() {return nomeMateria; /*this. e' opzionale*/}
+
+    public String[][][] getCalendario() {return calendario; /*this e' opzionale*/}
+    //---------------------------------------------------------------------------------------------------------------------------------
 }
