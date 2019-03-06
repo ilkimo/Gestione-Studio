868ba63f20c596d243bdf539ccc993fce89a25ce
Kim Viberti <vibertikim@yahoo.it>
2019-03-06 08:13:41 +0100

    Parser.java, finiti i metodi per il conto delle righe e la lettura di un File

--- Parser.java
+++ Parser.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.*;
 
 public class Parser {
-    private static int fileLength(String fileName) {
+    private static int fileLength(String fileName)throws FileNotFoundException, IOException {
         BufferedReader bf;
         int counter = 0;
 
@@ -14,34 +14,48 @@ public class Parser {
                     ++counter;
                 }
             }
-            catch (IOException e) {
-                System.out.println("IOException occurred");
-            }
+            catch (IOException e) {throw e;}
         }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
+        catch (FileNotFoundException e) {throw e;}
 
         return counter;
     }
 
-    private String[] readFile(String fileName) {
-        int fLength = 0;
+    private static String[] readFile(String fileName)throws FileNotFoundException, IOException {
+        int fLength = fileLength(fileName);
+        String[] res = new String[fLength];
 
         try {
-            fLength = fileLength(fileName);
             BufferedReader bf = new BufferedReader(new FileReader(fileName));
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
 
-        String[] res = new String[fLength];
+            for(int i = 0; i < fLength; ++i){
+                res[i] = bf.readLine();
+            }
+        }
+        catch (FileNotFoundException e) {throw e;}
+        catch (IOException e){throw e;}
 
         return res;
     }
 
-    public static void main(String[] args){
-        System.out.println("Numero di righe: " + fileLength("prova.txt"));
+    public static void main(String[] args){ //main di prova, cancellami poi
+        String nFile = "prova.txt";
+        String[] strFile;
+        try{
+            strFile = readFile(nFile);
+            System.out.println("Numero di righe: " + fileLength(nFile));
+
+            System.out.println("Ed il contenuto del file e': ");
+
+            for(int i = 0; i < fileLength(nFile); ++i){
+                System.out.println("    " + strFile[i]);
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch(IOException e) {
+            System.out.println("IOException");
+        }
     }
 }
