f2bca1d02aa54d02e3f9e51bb369b1d25a851a0e
Kim Viberti <vibertikim@yahoo.it>
2019-03-06 09:23:39 +0100

    Primi metodi inseriti

--- Materia.java
+++ Materia.java
@@ -1,4 +1,20 @@
 
 public class Materia {
-    
+    private int oreTot, oreTeor, orePrat;
+
+    public Materia() {
+
+    }
+
+    private void setOreTot() {
+
+    }
+
+    private void setOreTeor() {
+
+    }
+
+    private void setOrePrat() {
+
+    }
 }
