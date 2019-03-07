e8cd2af63ea680aca3ad9ba5e0519592c9857a96
Kim Viberti <vibertikim@yahoo.it>
2019-03-07 10:11:06 +0100

    little changes to a funcion

--- ParserMateria.java
+++ ParserMateria.java
@@ -1,16 +1,25 @@
+//anno\mese\giorno oraInizio:mntInizio-oraFine:mntFine oreEffettiveh minutiEffettivimnt "Materia"\ramo{Persona1, Persona2...}[luogo]
+//13 colonne (al momento)
 
 public class ParserMateria extends Parser {
     private String[] fileLines;
-
-    public ParserMateria() {}
+    //Constructors-----------------------------------------------------------------------------------------------------------------------
+    public ParserMateria() {fileLines = null;}
 
     public ParserMateria(String[] file) {fileLines = file; /*this. e' opzionale*/}
-
+    //get--------------------------------------------------------------------------------------------------------------------------------
     public String[] getFileLines() {return fileLines;}
-
-
-
+    //-----------------------------------------------------------------------------------------------------------------------------------
+    public void initializeSubject(Materia subject, String inputStr) throws FileLinesException {
+        if(fileLines != null) {
+            
+        } else {
+            throw new FileLinesException("Exception, trying to initialize calendar from fileLines == null");
+        }
+    }
+    //TRASH------------------------------------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
 
     }
+    //-----------------------------------------------------------------------------------------------------------------------------------
 }
